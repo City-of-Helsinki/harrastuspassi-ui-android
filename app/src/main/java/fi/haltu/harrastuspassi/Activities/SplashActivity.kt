920cba203facaf1d2ec4c255ac8721d455251219
Akio Ide <akio.ide@haltu.fi>
2019-07-03 14:20:56 +0300

    Add application icon

--- app/src/main/java/fi/haltu/harrastuspassi/Activities/SplashActivity.kt
+++ app/src/main/java/fi/haltu/harrastuspassi/Activities/SplashActivity.kt
@@ -3,11 +3,13 @@ package fi.haltu.harrastuspassi.Activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sleep(1000)
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
