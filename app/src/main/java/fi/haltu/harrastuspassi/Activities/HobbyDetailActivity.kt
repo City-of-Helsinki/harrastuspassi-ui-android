d31fb75ac621123bdf43d887709be87bd82223e7
Akio Ide <akio.ide@haltu.fi>
2019-07-03 16:47:38 +0300

    Add gesture desture listener on HobbyDetail screen. Whenever user swipes right, the application goes back to Main screen

--- app/src/main/java/fi/haltu/harrastuspassi/Activities/HobbyDetailActivity.kt
+++ app/src/main/java/fi/haltu/harrastuspassi/Activities/HobbyDetailActivity.kt
@@ -2,12 +2,18 @@ package fi.haltu.harrastuspassi.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.ImageView
 import android.widget.TextView
 import fi.haltu.harrastuspassi.Model.Hobby
 import fi.haltu.harrastuspassi.R
 
-class HobbyDetailActivity : AppCompatActivity() {
+
+
+class HobbyDetailActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     private lateinit var hobbyImage: ImageView
     private lateinit var hobbyTitle: TextView
     private lateinit var hobbyOrganizer: TextView
@@ -17,7 +23,7 @@ class HobbyDetailActivity : AppCompatActivity() {
     private lateinit var description: TextView
     private lateinit var enrolment: TextView
     private lateinit var organizer: TextView
-
+    private lateinit var gDetector: GestureDetectorCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobby_detail)
@@ -44,5 +50,58 @@ class HobbyDetailActivity : AppCompatActivity() {
         description.text = hobby.description
         enrolment.text = hobby.description
         organizer.text = hobby.organizer
+
+        this.gDetector = GestureDetectorCompat(this, this)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        this.gDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    private val swipeThreshhold = 100
+    private val swipeVelocityThreshhold = 100
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+
+        val diffY = e2!!.y - e1!!.y
+        val diffX = e2.x - e1.x
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (Math.abs(diffX) > swipeThreshhold && Math.abs(velocityX) > swipeVelocityThreshhold) {
+                if (diffX > 0) {
+                    onBackPressed()
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        Log.d("Touch detect","onDown")
+
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+        Log.d("Touch detect","onLongPress")
+
+    }
+
+    override fun onScroll(e1: MotionEvent, e2: MotionEvent,
+                          distanceX: Float, distanceY: Float): Boolean {
+        Log.d("Touch detect","onScroll")
+
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        Log.d("Touch detect","onShownPress")
+
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        Log.d("Touch detect","onSingleTapUp")
+
+        return true
     }
 }
