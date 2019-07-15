fb6603991ec871a0a1cfde45192e9891b766aca0
Akio Ide <akio.ide@haltu.fi>
2019-07-15 10:05:29 +0300

    Delete navigation bar

--- app/src/main/java/fi/haltu/harrastuspassi/Activities/MainActivity.kt
+++ app/src/main/java/fi/haltu/harrastuspassi/Activities/MainActivity.kt
@@ -1,38 +1,16 @@
 package fi.haltu.harrastuspassi.Activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
 import fi.haltu.harrastuspassi.R
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var button: Button
-
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
-
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
