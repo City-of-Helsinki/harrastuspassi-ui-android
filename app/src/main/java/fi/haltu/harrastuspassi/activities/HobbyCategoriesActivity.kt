1dfdb2be3c4ef045118b08c30171a05b44aa617e
Tiia Trogen <tiia.trogen@haltu.fi>
2019-08-13 17:25:45 +0300

    Ref: c-helsinki/harrastuspassi_android#3 Add base component for category filtering view and navigation to Hobby Categories list

--- app/src/main/java/fi/haltu/harrastuspassi/activities/HobbyCategoriesActivity.kt
+++ app/src/main/java/fi/haltu/harrastuspassi/activities/HobbyCategoriesActivity.kt
@@ -1,26 +1,16 @@
 package fi.haltu.harrastuspassi.activities
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import fi.haltu.harrastuspassi.R
 
-class FilterViewActivity : AppCompatActivity() {
+class HobbyCategoriesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_filter_view)
+        setContentView(R.layout.activity_hobby_categories)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator (R.drawable.ic_clear_black_24dp)
-        supportActionBar!!.title = "Suodata"
-
-    }
-
-    fun openCategories (view: View) {
-        val intent = Intent(this, HobbyCategoriesActivity::class.java).apply {
-
-        }
-        startActivity(intent)
+        supportActionBar!!.title = "Valitse harrastus"
     }
 }
