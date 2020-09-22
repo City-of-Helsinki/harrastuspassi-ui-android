36494d8f5a161e20e114ffac4350a208853336c5
Vohveli <reko.valtaoja@outlook.com>
2020-09-22 10:23:22 +0300

    Remove useless import and clean code

--- app/src/main/java/fi/haltu/harrastuspassi/fragments/HobbyEventListFragment.kt
+++ app/src/main/java/fi/haltu/harrastuspassi/fragments/HobbyEventListFragment.kt
@@ -3,7 +3,6 @@ package fi.haltu.harrastuspassi.fragments
 import android.annotation.SuppressLint
 import android.app.ActivityOptions
 import android.content.Intent
-import android.content.res.ColorStateList
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
